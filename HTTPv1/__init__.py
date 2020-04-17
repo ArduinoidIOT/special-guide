bc83240cd59a4435cf0f4492236242db3aeb573e
***** *** <rohroexperiment@rohroexperiment.com>
2020-04-17 14:00:51 +0530

    remove testing code

--- HTTPv1/__init__.py
+++ HTTPv1/__init__.py
@@ -1,15 +1,3 @@
 
 from MultiServer import Server
 from HTTPResponseGenerator import redirect, Response, NullResponse
-
-if __name__ == '__main__':
-    app = Server(addr='0.0.0.0',keep_alive=False)
-    @app.route('/')
-    def noob(req):
-        return "<h1>HEY NOOBS {}</h1>".format(req.addr)
-
-    # @app.errorhandler(404)
-    # def nobo(er):
-    #     return 404, "<h1>NOPE</h1>"
-
-    app.run()
